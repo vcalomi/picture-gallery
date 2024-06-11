51d066e48654fdb0dbf99de4ff20fede2d6acb76
vcalomino <vcalomino@fi.uba.ar>
2024-06-11 12:37:26 -0300

    add user to db

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ db.init_app(app)
 def main():
     return "<h1>Hello world</h1>"
 
+@app.route("/create/user", methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    if not data or not "username" in data or not "email" in data:
+        return jsonify({"message": "Missing fields"}), 400
+    
+    name = data["username"]
+    email = data["email"]
+    password = data["password"]
+
+    new_user = User(username=name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created succesfully"}), 201
+
 if __name__ == "__main__":
     print("Starting server...")
     with app.app_context():
