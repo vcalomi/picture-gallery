e9eb6729ecbe73d5b68075121b40f69d472fedcf
vcalomino <vcalomino@fi.uba.ar>
2024-06-11 12:53:40 -0300

    add created_at field for user

--- models/User.py
+++ models/User.py
@@ -5,6 +5,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
