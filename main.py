ad9c7184ddf5c9c9b0ee3a1b10b0dcf8b565071d
vcalomino <vcalomino@fi.uba.ar>
2024-06-11 12:36:26 -0300

    reestructure of project files

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from config import db
+from models.User import User
 
 app = Flask(__name__)
 port: int = 5000
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:0402@localhost/photo_gallery"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route("/")
 def main():
     return "<h1>Hello world</h1>"
 
-
-
-
-
-
 if __name__ == "__main__":
     print("Starting server...")
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", debug=True, port=port)
